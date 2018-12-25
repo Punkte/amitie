89d9227ab2ef2ef7cbdec8c49dbece7b8bc1dd45
Punkte <paartheepan94@gmail.com>
2018-12-25 20:23:20 +0100

    first redux commit

--- src/components/Discussion.js
+++ src/components/Discussion.js
@@ -34,42 +34,20 @@ const BubbleContainer = styled.div`
 class Discussion extends Component {
     constructor(props) {
         super(props)
-        this.el = []
-        this.container = null
-        this.tweenAnimation = null
     }
     
     componentDidMount() {
-        this.tweenAnimation = TweenLite.to( this.el, 0.3, {x: 0, y: 0, opacity: 1}).delay(1.5)
     }
     componentDidUpdate() {
-        this.el.forEach(div => {
-            if( div.hasAttribute('type') ) {
-                this.tweenAnimation = TweenLite.to( div, 0.2, {x: 0, y: 0, opacity: 1 })
-            }else {
-                this.tweenAnimation = TweenLite.to( div, 0.2, {x: 0, y: 0, opacity: 1 }).delay(1.8)
-            }
-            this.container.scrollTop = (this.container.scrollHeight)
-            
-        })
-        this.elements = []
     }
 
     render() {
         return(
             <div>
-                <BubbleContainer
-                    ref={ container => this.container = container }
-                    >
-                    { this.props.messages.map((message, index) => (
-                        <Bubble 
-                            key={ index } 
-                            ref={ el => this.el[index] = el }
-                            type={ !message || !message.type ? undefined : message.type }
-                            >
-                        { message.content }
+                <BubbleContainer>
+                        <Bubble>
+                            Text
                         </Bubble>
-                    )) }
                 </BubbleContainer>
             </div>
         )
